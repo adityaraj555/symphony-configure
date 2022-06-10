6dc14f715e487ca834f51b7dfb69c2f53cada9b9
Asfar Sharief <asfar.sharief@eagleview.com>
2022-06-10 16:25:28 +0530

    picking from env

--- stage/s3-config-map.tf
+++ stage/s3-config-map.tf
@@ -7,11 +7,11 @@ output "s3_config_map" {
   value = {
     "property-data-orchestrator" = {
       identifiers = [
-        "arn:aws:iam::105810807432:role/us-east-2-roofmodel-blue-hermes-instance",
-        "arn:aws:iam::105810807432:root",
-        "arn:aws:iam::105810807432:role/us-east-2-roofmodel-green-hermes-instance",
-        "arn:aws:iam::105810807432:role/EC2-Admin",
-        "arn:aws:iam::105810807432:role/measurement-service-lambda-LambdaExecutionRole-6V7XQJ542YDE"   
+        "arn:aws:iam::${local.evtech_factory_account}:role/us-east-2-roofmodel-blue-hermes-instance",
+        "arn:aws:iam::${local.evtech_factory_account}:root",
+        "arn:aws:iam::${local.evtech_factory_account}:role/us-east-2-roofmodel-green-hermes-instance",
+        "arn:aws:iam::${local.evtech_factory_account}:role/EC2-Admin",
+        "arn:aws:iam::${local.evtech_factory_account}:role/measurement-service-lambda-LambdaExecutionRole-6V7XQJ542YDE"   
       ]
       actions = [
       "s3:GetObject",
