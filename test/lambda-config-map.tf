4532acbeaca969db1fb9938be99abc582b480805
Shashikant Kurhade <shashikant.kurhade@SKURAD-BLRM20.local>
2022-08-25 10:02:33 +0530

    Changed hipster count

--- test/lambda-config-map.tf
+++ test/lambda-config-map.tf
@@ -255,7 +255,7 @@ output "lambda_configmap" {
       environment_variables = {
         "DBSecretARN" : "${local.property_data_orchestration_secret}",
         "SlackChannel" : "${local.slack_channel}",
-        "AllowedHipsterCount" : 0
+        "AllowedHipsterCount" : 2000
       }
       aws_lambda_permission = [
         "ec2.amazonaws.com"
