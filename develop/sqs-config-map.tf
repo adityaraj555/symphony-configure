bfdd7b76ae6f9d9e2d5f208c5e319cb22cb6645f
Satyanarayana Vadapalli <satyanarayana.v@eagleview.com>
2022-09-08 16:08:12 +0530

    Update sqs-config-map.tf

--- develop/sqs-config-map.tf
+++ develop/sqs-config-map.tf
@@ -60,16 +60,7 @@ EOF
      "Effect": "Allow",
      "Principal": {
          "Federated": "arn:aws:iam::${local.account_id}:oidc-provider/oidc.eks.us-east-2.amazonaws.com/id/${local.eks_cluster_id}"
-     },
-     {
-     "Action": "sts:AssumeRoleWithWebIdentity",
-     "Condition": {
-         "StringEquals": {
-             "oidc.eks.us-east-2.amazonaws.com/id/${local.eks_cluster_id}:sub": "system:serviceaccount:factory-dx-reports-workflow:factory-dx-reports-workflow-service-account",
-             "oidc.eks.us-east-2.amazonaws.com/id/${local.eks_cluster_id}:aud": "sts.amazonaws.com"
-         }
-    }
-    }
+     }
   
   ]
 }
