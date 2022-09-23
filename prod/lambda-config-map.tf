59ae8825dc585bf198f1b031b5837ab825114025
Asfar Sharief <asfar.sharief@eagleview.com>
2022-09-23 09:20:33 +0530

    Updated evml images

--- prod/lambda-config-map.tf
+++ prod/lambda-config-map.tf
@@ -235,7 +235,7 @@ output "lambda_configmap" {
       EOF
     },
     "${local.evmlconveter_lambda_name}" = {
-      image_uri          = "${local.ecr_path}/${local.evmlconveter_lambda_name}:78823e2.219"
+      image_uri          = "${local.ecr_path}/${local.evmlconveter_lambda_name}:0259828.222"
       lambda_handler     = null
       lambda_description = "Lambda"
       package_type       = "Image"
