53d1f150e0a760b244f4759d964de5d0db188bbc
Asfar Sharief <asfar.sharief@eagleview.com>
2022-10-04 16:28:47 +0530

    Update lambda-config-map.tf

--- test/lambda-config-map.tf
+++ test/lambda-config-map.tf
@@ -151,7 +151,7 @@ output "lambda_configmap" {
 
     },
     "${local.datastore_lambda_name}" = {
-      image_uri          = "${local.ecr_path}/${local.datastore_lambda_name}:11df2fe.211"
+      image_uri          = "${local.ecr_path}/${local.datastore_lambda_name}:3cc8789.276"
       vpc_id             = local.lambda_vpc_id,
       lambda_handler     = null
       lambda_description = "Lambda"
